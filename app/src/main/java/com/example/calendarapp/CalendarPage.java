7c1c3f9ad45e591f2498e6b45c4aa7b2f3c65203
Ben Szeto <benkszeto@gmail.com>
2022-11-09 19:43:51 -0500

    font and calendar page

--- app/src/main/java/com/example/calendarapp/CalendarPage.java
+++ app/src/main/java/com/example/calendarapp/CalendarPage.java
@@ -4,15 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import java.util.HashMap;
-import java.util.Map;
+public class CalendarPage extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
-
-    public Map<Integer, reminder> reminders = new HashMap();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_calendar_page);
     }
 }
